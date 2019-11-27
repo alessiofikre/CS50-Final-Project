ef4dd3b2e8e07c5a9ef2c0295d5c692626bae711
Alessio Fikre <alessiofikre@college.harvard.edu>
2019-11-27 18:18:43 -0500

    Created cells with mock information in the initial tableview

--- Syllabus Tracker v2/Syllabus Tracker v2/ViewController.swift
+++ Syllabus Tracker v2/Syllabus Tracker v2/ViewController.swift
@@ -13,8 +13,37 @@ class TableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+    let formatter = DateFormatter()
+    formatter.dateFormat = "MM/dd HH:mm"
+        
     }
-
-
+    
+    let classes = [
+        (classNumber: 1, className: "AM104"),
+        (classNumber: 2, className: "CS50"),
+        (classNumber: 3, className: "Tech Ethics")
+    ]
+    
+    let list = [
+        (classAssignment: "Final Project", deadline: "12/8 11:59 PM"),
+        (classAssignment: "Final Exam", deadline: "12/12 9:00 AM"),
+        (classAssignment: "Final Exam", deadline: "12/15 3:00 PM"),
+        ]
+        
+    override func numberOfSections(in tableView: UITableView) -> Int {
+            return 1
+        }
+        
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return list.count
+        }
+        
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MainAssignmentCell", for: indexPath)
+        cell.textLabel?.text = classes[indexPath.row].className + " " + list[indexPath.row].classAssignment
+        cell.detailTextLabel?.text = list[indexPath.row].deadline
+        return cell
+        }
+    
 }
-
